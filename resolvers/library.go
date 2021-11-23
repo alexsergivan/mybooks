3eaecd7c41d13e32ac40b9c3e4c097af57dd6a74
Oleksandr Ivanchenko <alexsergivan@gmail.com>
2021-11-23 13:34:06 +0100

    Added cache for the library

--- resolvers/library.go
+++ resolvers/library.go
@@ -2,6 +2,9 @@ package resolvers
 
 import (
 	"net/http"
+	"time"
+
+	"github.com/alexsergivan/mybooks/cache"
 
 	"github.com/alexsergivan/mybooks/userbook"
 	"github.com/labstack/echo/v4"
@@ -11,9 +14,21 @@ import (
 
 func GetLibrary(db *gorm.DB, storage *gormstore.Store) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		books := userbook.GetBooksListGroupedByLetter(db)
-		templateData := map[string]interface{}{
-			"books": books,
+		ristrettoCache := cache.NewRistrettoCache()
+		cacheKey := "library"
+		templateData := map[string]interface{}{}
+		library, found := ristrettoCache.Get(cacheKey)
+		if !found {
+			books := userbook.GetBooksListGroupedByLetter(db)
+			ristrettoCache.Set(cacheKey, books, time.Hour*24)
+			time.Sleep(10 * time.Millisecond)
+			templateData = map[string]interface{}{
+				"books": books,
+			}
+		} else {
+			templateData = map[string]interface{}{
+				"books": library.(map[string][]userbook.Book),
+			}
 		}
 
 		return c.Render(http.StatusOK, "books--library", templateData)
